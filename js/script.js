625a322c5350759e9a25e3429a316981b6b0eeda
Ney Pimenta <ney.pimenta.jr@gmail.com>
2012-08-03 00:41:05 -0400

    refactoring
    Use of HTML5's new semantic tags. Pure CSS instead of image maps.

--- js/script.js
+++ js/script.js
@@ -16,39 +16,4 @@ $(document).ready(function() {
 		$('#slider').anythingSlider(h.substring(1));			
 		return false;
 	});
-	
-	function wrapInParagraph(txt, price) {
-		return '<p>'+txt+'<br />$'+price+'</p><a href="http://www.rw-co.com/" target="_blank"><p><span>SHOP NOW<span><img src="images/black_arrow.gif" /></p></a>';
-	}
-					 
-	$('#picks_pic').mapster({
-		fillOpacity: 0.1,
-		fillColor: "d42e16",
-		mapKey: 'alt',
-		areas:  [
-			{
-		   key: "belt", 
-		   toolTip: wrapInParagraph('Red and gold belt','24.00')
-			},
-			{
-			key: "hoops",
-			toolTip: wrapInParagraph('Leafy gold hoops','10.00')
-			},
-			{
-			key: "sparkly_rings",
-			toolTip: wrapInParagraph('Stackable sparkly rings','10.00')
-			},
-			{
-			key: "rhinestone",
-			toolTip: wrapInParagraph('Rhinestone rings','12.00')
-			},
-			{
-			key: "earrings",
-			toolTip: wrapInParagraph('Feather drop earrings','10.00')
-			}
-		],
-		showToolTip: true,			
-		//toolTipContainer: '<div class="tooltip">...</div>',
-		toolTipClose: ["tooltip-click"]
-	});	
 });
